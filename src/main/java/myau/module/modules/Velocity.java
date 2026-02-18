f729a55b4835bf00a237f96890d56cc505fcf363
spirit <spirit.business15@gmail.com>
2026-02-18 23:23:13 +1100

    Create Velocity.java

---
+++ src/main/java/myau/module/modules/Velocity.java
@@ -0,0 +1,69 @@
+package myau.module.modules;
+
+import myau.Myau;
+import myau.event.EventTarget;
+import myau.events.KnockbackEvent;
+import myau.mixin.IAccessorEntity;
+import myau.module.Module;
+import myau.property.properties.BooleanProperty;
+import net.minecraft.client.Minecraft;
+
+public class Velocity extends Module {
+
+    private static final Minecraft mc = Minecraft.getMinecraft();
+
+    public final BooleanProperty fakeCheck = new BooleanProperty("fake-check", true);
+    public final BooleanProperty debugLog = new BooleanProperty("debug-log", false);
+
+    private boolean pendingExplosion = false;
+
+    public Velocity() {
+        super("Velocity", false);
+    }
+
+    private boolean isInLiquidOrWeb() {
+        return mc.thePlayer.isInWater()
+                || mc.thePlayer.isInLava()
+                || ((IAccessorEntity) mc.thePlayer).getIsInWeb();
+    }
+
+    private boolean canDelay() {
+        KillAura killAura = (KillAura) Myau.moduleManager.modules.get(KillAura.class);
+        Autoblock autoblock = (Autoblock) Myau.moduleManager.modules.get(Autoblock.class);
+
+        return mc.thePlayer.onGround
+                && (!killAura.isEnabled() || !autoblock.isBlocking());
+    }
+
+    @EventTarget
+    public void onKnockback(KnockbackEvent event) {
+        if (!this.isEnabled()) return;
+
+        // Cancel if explosion knockback is pending
+        if (this.pendingExplosion) {
+            event.setCancelled(true);
+            this.pendingExplosion = false;
+            return;
+        }
+
+        // Cancel if player is in liquid or web
+        if (this.fakeCheck.getValue() && this.isInLiquidOrWeb()) {
+            event.setCancelled(true);
+            return;
+        }
+
+        // Delay knockback if autoblock or killaura conditions require it
+        if (this.canDelay()) {
+            event.setCancelled(true);
+            return;
+        }
+
+        // Debug logging
+        if (this.debugLog.getValue()) {
+            System.out.println("[Velocity] Knockback event: "
+                    + "motionX=" + event.getMotionX()
+                    + ", motionY=" + event.getMotionY()
+                    + ", motionZ=" + event.getMotionZ());
+        }
+    }
+}
