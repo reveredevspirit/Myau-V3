84e3846b076f5f9ddbcd65f4bb7ad27d3267b533
spirit <spirit.business15@gmail.com>
2026-02-20 08:47:49 +1100

    Update ScissorUtil.java

--- src/main/java/myau/ui/clickgui/ScissorUtil.java
+++ src/main/java/myau/ui/clickgui/ScissorUtil.java
@@ -6,17 +6,24 @@ import org.lwjgl.opengl.GL11;
 
 public class ScissorUtil {
 
+    private static final Minecraft mc = Minecraft.getMinecraft();
+
     public static void enable(int x, int y, int width, int height) {
-        ScaledResolution sr = new ScaledResolution(Minecraft.getMinecraft());
-        int scale = sr.getScaleFactor();
-        int screenH = Minecraft.getMinecraft().displayHeight;
+        ScaledResolution sr = new ScaledResolution(mc);
+        int scale   = sr.getScaleFactor();
+        int screenH = mc.displayHeight;
+
+        int clampedX = Math.max(0, x);
+        int clampedY = Math.max(0, y);
+        int clampedW = Math.max(0, width);
+        int clampedH = Math.max(0, height);
 
         GL11.glEnable(GL11.GL_SCISSOR_TEST);
         GL11.glScissor(
-                x * scale,
-                screenH - (y + height) * scale,
-                width * scale,
-                height * scale
+                clampedX * scale,
+                screenH - (clampedY + clampedH) * scale,
+                clampedW * scale,
+                clampedH * scale
         );
     }
 
